6e137a6168f347b8efc84d9ff76a8c31544a9c40
Ian Warwick <ian.warwick@justeattakeaway.com>
2024-05-04 19:38:24 +0100

    some jiggery pokery to get it to work in intellij with different arg lengths

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,39 +7,45 @@
 import com.fluxtah.ask.app.App
 
 fun main(args: Array<String>) {
-    var interactive = false
-    if (args.size > 1) {
-        when {
-            args[1] == "--version" -> {
-                println("0.11")
-                return
-            }
-            args[1] == "--help" -> {
-                println("Usage: ask [options]")
-                println("Ask is a command line tool for interacting with OpenAI's Assistants API")
-                println()
-                println("Options:")
-                println("  --version        Print the version of the application")
-                println("  --help           Print this help message")
-                println("  --interactive    Run ask in interactive mode")
-                println("  <command>  Run a command in ask, run in interactive mode for available commands to setup first")
-                return
-            }
-            args[1] == "--interactive" -> {
-                interactive = true
-            }
+    val interactive = args.contains("--interactive")
+    val help = args.contains("--help")
+    val version = args.contains("--version")
+    val startsWithProgramName = args.firstOrNull()?.startsWith("ask") ?: false
+
+    when {
+        version -> {
+            println("0.11")
+            return
+        }
+
+        help -> {
+            println("Usage: ask [options]")
+            println("Ask is a command line tool for interacting with OpenAI's Assistants API")
+            println()
+            println("Options:")
+            println("  --version        Print the version of the application")
+            println("  --help           Print this help message")
+            println("  --interactive    Run ask in interactive mode")
+            println("  <command>  Run a command in ask, run in interactive mode for available commands to setup first")
+            return
         }
     }
 
     val app = App()
 
-    if(interactive) {
+    if (interactive) {
         app.run()
+        return
+    }
+
+    if (!startsWithProgramName) {
+        app.runOneShotCommand(args.joinToString(" "))
+        return
+    }
+
+    if (args.size < 2) {
+        println("Usage: ask <command>")
     } else {
-        if(args.size < 2) {
-            println("Usage: ask <command>")
-            return
-        }
         app.runOneShotCommand(args.drop(1).joinToString(" "))
     }
 }
