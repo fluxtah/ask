c61300bdcf42745923fc7a15b7164f4567dc5adc
Ian Warwick <ian.warwick@justeattakeaway.com>
2024-05-13 23:30:24 +0100

    short args

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,10 +9,10 @@ import com.fluxtah.ask.app.ConsoleApplication
 import java.io.File
 
 fun main(args: Array<String>) {
-    val testing = args.contains("--test-plugin")
-    val interactive = args.contains("--interactive")
-    val help = args.contains("--help")
-    val version = args.contains("--version")
+    val testing = args.contains("--test-plugin") || args.contains("-t")
+    val interactive = args.contains("--interactive") || args.contains("-i")
+    val help = args.contains("--help") || args.contains("-h")
+    val version = args.contains("--version") || args.contains("-v")
     val startsWithProgramName = args.firstOrNull()?.startsWith("ask") ?: false
 
     when {
@@ -26,10 +26,11 @@ fun main(args: Array<String>) {
             println("Ask is a command line tool for interacting with OpenAI's Assistants API")
             println()
             println("Options:")
-            println("  --version        Print the version of the application")
-            println("  --help           Print this help message")
-            println("  --interactive    Run ask in interactive mode")
-            println("  <command>        Run a command in ask, run in interactive mode for available commands to setup first")
+            println("  --version, -v        Print the version of the application")
+            println("  --help, -h           Print this help message")
+            println("  --interactive, -i    Run ask in interactive mode")
+            println("  --test-plugin, -t    Test a plugin")
+            println("  <command>            Run a command in ask, run in interactive mode for available commands to setup first")
             return
         }
     }
@@ -39,12 +40,12 @@ fun main(args: Array<String>) {
     if (testing) {
         val testPluginArgIndex = args.indexOf("--test-plugin")
         val testPluginFilePath = args.getOrNull(testPluginArgIndex + 1)
-        if(testPluginFilePath == null) {
+        if (testPluginFilePath == null) {
             println("Usage: ask --test-plugin <plugin-name>")
             return
         } else {
             val pluginFile = File(testPluginFilePath)
-            if(!pluginFile.exists()) {
+            if (!pluginFile.exists()) {
                 println("Plugin not found: $testPluginFilePath")
                 return
             }
